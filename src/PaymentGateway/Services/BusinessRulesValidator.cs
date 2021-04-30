5dfac2aeeb1fcf86708af36c17bb09c0e00944da
GEOPAPRAM <geopapram@gmail.com>
2021-04-30 10:46:48 +0100

    Fixed syntactic mistakes in comment

--- src/PaymentGateway/Services/BusinessRulesValidator.cs
+++ src/PaymentGateway/Services/BusinessRulesValidator.cs
@@ -17,9 +17,9 @@ namespace PaymentGateway.Services
                 yield return "Amount cannot be negative";
 
             //TODO: Here we keep it simple, but we may want to validate other business 
-            // rules, like for instance different maximum abount per currency etc.
-            // All such checks and limitation can be implemented as buseness rules.
-            // If rules become complems new BusinessRule class can be introduced.
+            // rules, like for instance different maximum amount per currency etc.
+            // All such checks and limitations can be implemented as buseness rules.
+            // If rules become complex a new BusinessRule class can be introduced.
         }
     }
 }
